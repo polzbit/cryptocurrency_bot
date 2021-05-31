5c546fb425b9187bbdf871a752d646c4922d0871
polzbit <38733198+polzbit@users.noreply.github.com>
2021-05-31 16:22:40 +0300

    Add files via upload

---
+++ test.py
@@ -0,0 +1,19 @@
+from cryptocurrency_bot import CrypoCurrencyBot
+from time import sleep
+
+class Tester:
+    def __init__(self):
+        self.currencies = ["bitcoin", "litecoin", "dogecoin"]
+    
+    # function to print data
+    def print_currencies(self):
+        # iterate over this list and get the data currency by currency
+        for currency in self.currencies:
+            c = CrypoCurrencyBot(currency)
+            print(currency + " Value: " + c.get_currency())
+            sleep(1)
+
+if __name__ == "__main__":
+    t = Tester()
+    # c.get_currency("bitcoin")
+    t.print_currencies()
