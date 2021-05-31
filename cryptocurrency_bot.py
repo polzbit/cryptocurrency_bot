fd3e54f813864a735835639e19af2c7142e55d72
polzbit <38733198+polzbit@users.noreply.github.com>
2021-05-31 17:04:53 +0300

    Update cryptocurrency_bot.py

--- cryptocurrency_bot.py
+++ cryptocurrency_bot.py
@@ -6,7 +6,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-import pandas as pd
+# import pandas as pd
 
 class CrypoCurrencyBot:
     def __init__(self, currency):
